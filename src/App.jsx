690294be35b2edc0837917bf63138ed447b620b6
UrielCordero <urielpro850@gmail.com>
2025-04-16 09:08:08 -0300

    Empezamos y seguimos

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+//import { useState } from 'react'
+import './App.css'
+import Form from './Formulario.jsx'
+
+function App() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Form></Form>
+    </>
+  )
+}
+
+
+
+export default App
---
+++ src/Formulario.jsx
@@ -0,0 +1,34 @@
+function Form() {
+
+    return <>
+    <form id="formulario" >
+        <h1>Crear mi cita:</h1>
+        <label>Nombre mascota:   
+            <input type="text" name="mascota" class="u-full-width" placeholder="Nombre Mascota" value=""/>
+        </label>
+    
+        
+        <label>Nombre dueño:
+            <input type="text" name="propietario" class="u-full-width" placeholder="Nombre dueño de la mascota" value=""/>
+        </label>
+        
+        <label>Fecha:
+            <input type="date" name="fecha" class="u-full-width" value=""/>
+        </label>
+        
+        <label>Hora:
+            <input type="time" name="hora" class="u-full-width" value=""/>
+        </label>
+  
+        <label>Sintomas:
+            <textarea name="sintomas" class="u-full-width"></textarea>
+        </label>
+  
+  
+        <button type="submit">Agregar cita</button>
+    </form>
+    </>
+  
+}
+
+export default Form
